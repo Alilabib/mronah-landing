ff0b39da740908a9f96ac4b97b8b8ff78641a401
Ali Labib <alilabib65@gmail.com>
2021-08-18 00:42:25 +0200

    update login and register

---
+++ javascript/index.js
@@ -0,0 +1,19 @@
+document.getElementById('lang').addEventListener('click',function(e){
+    e.preventDefault();
+    var language = document.getElementById('lang');
+    var body = document.getElementsByTagName("BODY")[0];
+    var logo = document.getElementById('logo');
+    if(language.innerHTML ==='EN'){
+        body.style.direction = 'ltr';
+        language.innerHTML = 'AR';
+        if(logo != null){
+            logo.src = "./images/mronah_en.png";
+        }
+    }else{
+        body.style.direction = 'rtl';
+        language.innerHTML = 'EN';
+        if(logo != null){
+            logo.src = "./images/mronah_ar.png";
+        }
+    }
+});
